79c7dc98d08be33813397b7e46964ad267b4f0f7
yixuewang <yixuewang@yixuedeair>
2018-11-19 22:25:05 -0500

    revert

--- first/src/first/main/BootStrap.java
+++ first/src/first/main/BootStrap.java
@@ -4,13 +4,11 @@ public class BootStrap {
 
 	public static void main(String[] args) {
 		
-		System.out.println("this is a second conflict");
+		System.out.println("hey i am coming");
 	 
-		System.out.println("this is a conflict");
+		System.out.println("second commit");
 	
 		System.out.println("version 1.1");
-		
-		System.out.println("I make changes");
 		
 	}
 	
