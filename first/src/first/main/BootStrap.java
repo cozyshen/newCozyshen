9b33548e54291d2389315af08b96fd1c6acddaa2
shen <shen@192.168.234.1>
2018-12-11 23:50:04 -0400

    branch commit 2

--- first/src/first/main/BootStrap.java
+++ first/src/first/main/BootStrap.java
@@ -3,31 +3,33 @@ package first.main;
 public class BootStrap {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("hey i am coming");
-	 
+
 		System.out.println("second commit");
-	
+
 		System.out.println("version 1.1");
-		
+
 	}
-	
+
 	public int doSomething(int a, int b) {
 
-		int c=a%b%a%b%a%b+a+a;
+		int c = a % b % a % b % a % b + a + a;
 		return c;
 	}
-	
+
 	public int createBeforeCreateBranch(int a, int b) {
 
-		int c=a+b+a+b+a+b/b/b/b/b/b/b;
+		int c = a + b + a + b + a + b / b / b / b / b / b / b;
 
 		return c;
 	}
-	
+
 	public void branchcommit1() {
-		
+
 	}
-	
-	
+
+	public void branchcommit2() {
+
+	}
 }
